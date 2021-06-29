07aeab27c47977720ea171608fb4547be9d5cdfa
deltagare <iogunjobi@gmail.com>
2021-06-29 07:23:04 +0200

    init

---
+++ src/main/java/se/lexicon/sanya/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.sanya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
